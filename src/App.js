715b3b29cbf716857be8335de832e5b512fa82c6
heep-jay <jubrilmuritala69@gmail.com>
2022-07-20 07:51:37 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ const App = () => {
   const [coordinates, setCoordinates] = useState({});
   const [bounds, setBounds] = useState(null);
   const [places, setPlaces] = useState([]);
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     navigator?.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
@@ -22,11 +24,12 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    console.log(coordinates, bounds)
+    setIsLoading(true)
     getPlacesData(bounds?.sw, bounds?.ne)
     .then((data) => {
     
       setPlaces(data)
+      setIsLoading(false)
     })
   
   }, [coordinates, bounds])
@@ -38,13 +41,15 @@ const App = () => {
       <Header/>
       <Grid container spacing={3} style={{width:"100%"}}>
         <Grid item xs={12} md={4}>
-          <List places={places}/>
+          <List places={places} childClicked={childClicked} isLoading={isLoading}/>
         </Grid>
         <Grid item xs={12} md={8}>
           <Map 
           setCoordinates={setCoordinates}
           setBounds={setBounds}
           coordinates={coordinates}
+          places={places}
+          setChildClicked={setChildClicked}
           />
         </Grid>
 
